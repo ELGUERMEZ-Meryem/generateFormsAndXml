5118981f28e856593e83671805d239776661dd05
Meryem El Guermez <elguermez.meryem@gmail.com>
2020-10-05 17:41:19 +0100

    fix Persistent entity  should have primary key

--- api/src/main/java/generateFormsXml/api/entity/AbstractEntity.java
+++ api/src/main/java/generateFormsXml/api/entity/AbstractEntity.java
@@ -3,8 +3,10 @@ package generateFormsXml.api.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.*;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
